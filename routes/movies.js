fa90117f35f86351baa68aa5a84bfe66bac0d43f
prasadbidwai <prasadbidwai@gmail.com>
2016-12-12 10:22:24 -0800

    removing unnecessary files

--- routes/movies.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var mongoose = require('../lib/mongodb');
-
-var movieSchema = require('../lib/movies.js');
-var Movie = mongoose.model('Movie', movieSchema);
-	
-module.exports = router;
